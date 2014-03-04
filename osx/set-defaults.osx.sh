267ce09182814a7a38ad4a675bbf01a7acab0020
Ankush Agarwal <ankushagarwal11@gmail.com>
2014-03-04 14:59:18 -0800

    Because tial is also tail

--- osx/set-defaults.osx.sh
+++ osx/set-defaults.osx.sh
@@ -8,7 +8,7 @@
 # Run ./set-defaults.sh and you'll be good to go.
 
 # Disable press-and-hold for keys in favor of key repeat.
-defaults write -g ApplePressAndHoldEnabled -bool true
+defaults write -g ApplePressAndHoldEnabled -bool false
 
 # Use AirDrop over every interface. srsly this should be a default.
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -22,3 +22,5 @@ alias dotfiles="subl ~/.dotfiles"
 
 # Pipe my public key to my clipboard.
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+
+alias tial='tail'
