b6af7c1b81da563e6ca6d01d7525982af2c9f46b
Ankush Agarwal <ankushagarwal11@gmail.com>
2014-03-06 12:24:27 -0800

    Move history options to the end

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,3 +1,5 @@
+# Everthing here is executed at the end
+
 # matches case insensitive for lowercase
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
@@ -15,3 +17,15 @@ fi
 
 #rvm related stuff
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+# Set these nice options at last
+set +o extended_history
+set +o histignorespace
+set +o histverify
+export HISTFILE=$HOME/.dotfiles/.history/history
+set -o APPEND_HISTORY
+export HISTSIZE=50000
+export SAVEHIST=50000
+set -o HIST_EXPIRE_DUPS_FIRST
+set +o extendedhistory
+set -o INC_APPEND_HISTORY
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -6,18 +6,6 @@ export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 #Configure zsh here
 
 #History related options
-export STSIZE=5000000
-export SAVEHIST=5000000
-export HISTFILE=~/.history
-export HISTCONTROL=ignoredups
-setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
-setopt EXTENDED_HISTORY # add timestamps to history
-setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY # adds history incrementally
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
-setopt HIST_REDUCE_BLANKS
-
 
 export OH_MY_ZSH=$HOME/.oh-my-zsh
 export ZSH_THEME="kphoen"
