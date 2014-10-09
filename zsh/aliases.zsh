79b03806810e8d4db54bc09f6de0bfccda2b037f
Ankush Agarwal <ankushagarwal11@gmail.com>
2014-10-09 00:32:24 -0700

    Add randomizeWiFi

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -40,3 +40,4 @@ alias dnsflush='sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder; su
 alias gerp='grep'
 alias classes='cd /Users/ankushagarwal/code/archive/python-projects/Classes'
 alias cds='/Users/ankushagarwal/.pyenv/versions/2.7.8/bin/cdiff -s'
+alias randomizeWiFi='sudo spoof-mac randomize Wi-Fi'
