f0b19a8d5b5a9f0e6b42804afb5310b8e8c25bc4
Ankush Agarwal <ankushagarwal11@gmail.com>
2014-12-24 23:17:52 -0800

    Better git commits

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -4,10 +4,8 @@ alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 #alias gd='cdiff -s'
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias gcb='git copy-branch-name'
+alias gc='git commit --verbose'
+alias gca='git commit --verbose -a'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
@@ -32,3 +30,8 @@ function gitrebase() {
   git fetch origin;
   git rebase origin/master;
 }
+
+if [[ -e /Users/ankushagarwal/.pyenv/shims/cdiff ]]; then
+  alias gd='/Users/ankushagarwal/.pyenv/shims/cdiff -s'
+  alias gdc='/Users/ankushagarwal/.pyenv/shims/cdiff -s --cached'
+fi
