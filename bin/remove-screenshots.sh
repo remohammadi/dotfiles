0c0204a8ed5399ea41f3193b63cae25e4c1121cd
Ankush Agarwal <ankushagarwal11@gmail.com>
2014-12-30 15:29:37 -0800

    Use print0 to avoid whitespace in filename

--- bin/remove-screenshots.sh
+++ bin/remove-screenshots.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/local/bin/find /Users/ankushagarwal/Documents/ScreenShots  -type f -mmin +2000 | /usr/local/bin/xargs --no-run-if-empty rm
+/usr/local/bin/find /Users/ankushagarwal/Documents/ScreenShots  -type f -mmin +2000 -print0 | /usr/local/bin/xargs -0 --no-run-if-empty rm
