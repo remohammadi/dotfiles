9f4ecb363c56a8f37bc1cdcea3b444b207da377c
Ankush Agarwal <ankushagarwal11@gmail.com>
2015-01-02 03:46:16 -0800

    M  remove-screenshots.sh

--- bin/remove-screenshots.sh
+++ bin/remove-screenshots.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 /usr/local/bin/find /Users/ankushagarwal/Documents/ScreenShots  -type f -mmin +2000 -print0 | /usr/local/bin/xargs -0 --no-run-if-empty rm
+mv /Users/ankushagarwal/Desktop/*.png /Users/ankushagarwal/.Trash
+mv /Users/ankushagarwal/Desktop/*.jpg /Users/ankushagarwal/.Trash
+mv /Users/ankushagarwal/Desktop/*jpeg /Users/ankushagarwal/.Trash
+mv /Users/ankushagarwal/Desktop/*JPG /Users/ankushagarwal/.Trash
+mv /Users/ankushagarwal/Desktop/*JPEG /Users/ankushagarwal/.Trash
+mv /Users/ankushagarwal/Downloads/*.png /Users/ankushagarwal/.Trash
+mv /Users/ankushagarwal/Downloads/*.jpg /Users/ankushagarwal/.Trash
+mv /Users/ankushagarwal/Downloads/*jpeg /Users/ankushagarwal/.Trash
+mv /Users/ankushagarwal/Downloads/*JPG /Users/ankushagarwal/.Trash
+mv /Users/ankushagarwal/Downloads/*JPEG /Users/ankushagarwal/.Trash
+exit 0
