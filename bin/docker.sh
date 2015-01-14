82e6f6218b34c579ec965db9c69b6086163aa375
Ankush Agarwal <ankushagarwal11@gmail.com>
2015-01-14 13:43:55 -0800

    Don't remove my containers

--- bin/docker.sh
+++ bin/docker.sh
@@ -6,7 +6,7 @@ export DOCKER_HOST="tcp://192.168.59.103:2376"
 export PATH=$HOME/.pyenv/shims:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:/usr/X11/bin:$HOME/.dotfiles/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
 status=`boot2docker status`
 if [ "$status" = "running" ]; then
-	docker ps -a | grep -e 'days ago' -e 'hours ago' | grep 'Exited ' | awk '{print $1}' | xargs --no-run-if-empty docker rm
+	#docker ps -a | grep -e 'days ago' -e 'hours ago' | grep 'Exited ' | awk '{print $1}' | xargs --no-run-if-empty docker rm
   docker images | grep '<none>                              <none>' | grep 'days ago' | awk '{print $3}' | xargs --no-run-if-empty docker rmi
 fi
 
