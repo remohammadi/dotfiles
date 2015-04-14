8e2f4cc37962d9f3e10867a6ba1d41d900e28407
Ankush Agarwal <ankushagarwal11@gmail.com>
2015-04-14 13:26:25 -0700

    Add dockerclean

--- bin/docker.sh
+++ bin/docker.sh
@@ -2,12 +2,12 @@
 date
 export DOCKER_CERT_PATH=$HOME/.boot2docker/certs/boot2docker-vm
 export DOCKER_TLS_VERIFY=1
-export DOCKER_HOST="tcp://192.168.59.103:2376"
+export DOCKER_HOST="tcp://192.168.59.104:2376"
 export PATH=$HOME/.pyenv/shims:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:/usr/X11/bin:$HOME/.dotfiles/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
 status=`boot2docker status`
 if [ "$status" = "running" ]; then
-	#docker ps -a | grep -e 'days ago' -e 'hours ago' | grep 'Exited ' | awk '{print $1}' | xargs --no-run-if-empty docker rm
-  docker images | grep '<none>                              <none>' | grep 'days ago' | awk '{print $3}' | xargs --no-run-if-empty docker rmi
+	docker ps -a | grep -e 'days ago' -e 'hours ago' | grep 'Exited ' | awk '{print $1}' | xargs --no-run-if-empty docker rm
+  docker images | egrep '<none>\ +<none>' | awk '{print $3}' | xargs --no-run-if-empty docker rmi
 fi
 
 images=`docker images | grep -v '<none> *<none>' | grep -v REPOSITORY | awk '{print $1}' | sort | uniq`
---
+++ bin/dockerclean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export DOCKER_CERT_PATH=$HOME/.boot2docker/certs/boot2docker-vm
+export DOCKER_TLS_VERIFY=1
+export DOCKER_HOST="tcp://192.168.59.104:2376"
+export PATH=$HOME/.pyenv/shims:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:/usr/X11/bin:$HOME/.dotfiles/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
+
+status=`boot2docker status`
+if [ "$status" = "running" ]; then
+  docker ps -a | grep -e 'days ago' -e 'hours ago' | grep 'Exited ' | awk '{print $1}' | xargs --no-run-if-empty docker rm
+  docker images | egrep '<none>\ +<none>' | awk '{print $3}' | xargs --no-run-if-empty docker rmi
+fi
