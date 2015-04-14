a49b5ccebfb60a9589a72abbd8d642399ac91295
Ankush Agarwal <ankushagarwal11@gmail.com>
2015-04-14 16:30:15 -0700

    M  bin/docker.sh M  bin/dockerclean.sh M  brew/brew-list M  docker/docker.zsh

--- bin/dockerclean.sh
+++ bin/dockerclean.sh
@@ -2,7 +2,7 @@
 
 export DOCKER_CERT_PATH=$HOME/.boot2docker/certs/boot2docker-vm
 export DOCKER_TLS_VERIFY=1
-export DOCKER_HOST="tcp://192.168.59.104:2376"
+export DOCKER_HOST="tcp://192.168.59.103:2376"
 export PATH=$HOME/.pyenv/shims:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:/usr/X11/bin:$HOME/.dotfiles/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
 
 status=`boot2docker status`
